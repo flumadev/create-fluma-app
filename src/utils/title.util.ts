bbd4f06c30b881de22beff68d8f57fbb7be7b791
Luis Amorim <luisfelipeamorim@hotmail.com>
2022-08-31 16:54:59 -0300

    Style(theme): Change title color

--- src/utils/title.util.ts
+++ src/utils/title.util.ts
@@ -2,12 +2,8 @@ import gradient from 'gradient-string';
 import TITLE_TEXT from '../config/text.config.js';
 
 const poimandresTheme = {
-  blue: '#add7ff',
-  cyan: '#89ddff',
-  green: '#5de4c7',
-  magenta: '#fae4fc',
-  red: '#d0679d',
-  yellow: '#fffac2',
+  purple: '#FAAEFC',
+  blue: '#12DFF3',
 };
 
 export const renderTitle = () => {
